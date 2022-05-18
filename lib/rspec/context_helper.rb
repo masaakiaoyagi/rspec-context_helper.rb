1c96edeaeb9cc1f752a4c1a5fd004f21000376ee
Masaaki Aoyagi <masaaki.aoyagi@gmail.com>
2022-05-18 20:34:16 +0900

    init
    $ bundle gem --mit --coc --test=rspec --ci=github --linter=rubocop
    rspec-context_helper

---
+++ lib/rspec/context_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "context_helper/version"
+
+module Rspec
+  module ContextHelper
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rspec/context_helper_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rspec::ContextHelper do
+  it "has a version number" do
+    expect(Rspec::ContextHelper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
